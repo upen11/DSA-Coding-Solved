8360e3a44ee42b8b8db368530e7b9425458fe535
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-09-15 19:45:34 +0530

    Time: 2 ms (45.85%), Space: 47 MB (84.48%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -5,16 +5,13 @@ class Solution {
         // [0,0,1,1,1,2,2,3,3,4]
         for(int i=1; i<nums.length; i++) {
             
-            if(nums[i] == nums[i-1]) {
-                continue;
-            }
-            else {
+            if(nums[i] > nums[i-1]) {
                 nums[++p1] = nums[i];
             }
         }
             
-            for(int i: nums)
-                System.out.print(i+" ");
+            // for(int i: nums)
+            //     System.out.print(i+" ");
             
             return p1+1;
         

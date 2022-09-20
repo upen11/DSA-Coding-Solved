848aaa2529401e1bf51ab38b0a020d827e5cde08
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-09-21 02:10:38 +0530

    Time: 22 ms (60.22%), Space: 41.3 MB (19.47%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -2,25 +2,22 @@
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
+
     public int firstBadVersion(int n) {
-        
         int start = 1;
         int end = n;
-        
-        while(start <= end) {
+        int ans = -1;
+        while (start <= end) {
             int mid = start + (end - start) / 2;
-            
-            if(isBadVersion(mid)) {
-                if(!isBadVersion(mid-1) && isBadVersion(mid+1))
-                    return mid;
-                else
-                    end = mid - 1;
-            }
-            else {
+
+            if (isBadVersion(mid)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
                 start = mid + 1;
             }
         }
-        
-        return start;
+
+        return ans;
     }
 }

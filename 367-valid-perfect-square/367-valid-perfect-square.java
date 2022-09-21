857e22f13c12435de0c04a281c3636a39ccd1e6b
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-09-21 10:55:34 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (92.40%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -2,8 +2,8 @@ class Solution {
 
     public boolean isPerfectSquare(int num) {
         int start = 0;
-        int end = num;
-        long mid = 0;
+        int end = 46340;   //sq root of 2^31. This is the max a number can be.
+        int mid = 0;
         boolean found = false;
         while (start <= end) {
             mid = start + (end - start) / 2;
@@ -12,9 +12,9 @@ class Solution {
                 found = true;
                 break;
             } else if (mid * mid < num) {
-                start = (int)mid + 1;
+                start = mid + 1;
             } else {
-                end = (int)mid - 1;
+                end = mid - 1;
             }
         }
 

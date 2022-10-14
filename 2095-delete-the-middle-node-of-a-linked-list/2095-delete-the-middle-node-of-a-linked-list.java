a2e935d49c5b535f0e8389b4310741cdc0f834db
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-10-14 23:47:41 +0530

    Time: 7 ms (51.20%), Space: 212 MB (65.51%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -14,25 +14,22 @@ class Solution {
         if (head == null) {
             return null;
         }
-
+        
+        // only one element is there [2], return empty/null
         if (head.next == null) {
             return null;
         }
 
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode slowPointer = dummy;
-        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+        ListNode fastPointer = head.next.next;  // we have checked head.next == null condition above
+        
+        // before checking the loop we have already placed slow and fastPointer on 0th and 2nd index
 
-        while (fastPointer.next != null) {
+        while (fastPointer != null && fastPointer.next != null) {   // fastPointer will be null when list is [1, 2]
             slowPointer = slowPointer.next;
-            if (fastPointer.next.next != null) {
-                fastPointer = fastPointer.next.next; }
-            else {
-                break;
-            }
+            fastPointer = fastPointer.next.next;
         }
-
+        
         slowPointer.next = slowPointer.next.next;
 
         return head;

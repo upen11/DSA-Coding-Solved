fd3daafd2442e94cb1e014b7415f675245c39d28
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-10-18 22:10:56 +0530

    Time: 4 ms (85.69%), Space: 41.6 MB (80.31%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,54 +1,40 @@
 class Solution {
+
     public String countAndSay(int n) {
-        
-        if(n==1) {
+        if (n == 1) {
             return "1";
         }
-        
-        if(n==2) {
-            return "11";
+
+        String ans = "1";
+        for (int x = 2; x <= n; x++) {
+            ans = count(ans);
         }
-        
-        String s = "11";
-        String ans = s;
-        
-        int x = 2;
-        while(++x <= n) {
-            s = count(s);
-            ans = s;
-        }
-        
+
         return ans;
-        
     }
 
     public String count(String num) {
-
-        
         StringBuilder sb = new StringBuilder();
-        
+
         int count = 1;
-        
+
         int i = 0;
-        for(i=0; i<num.length()-1; i++) {
+        for (i = 0; i < num.length() - 1; i++) {
             char ch = num.charAt(i);
-            char ch1 = num.charAt(i+1);
-            
-            if(ch == ch1) {
+            char ch1 = num.charAt(i + 1);
+
+            if (ch == ch1) {
                 count++;
-            }
-            else {
+            } else {
                 sb.append(count);
                 sb.append(ch);
                 count = 1;
-            }   
+            }
         }
 
         sb.append(count);
-        sb.append(num.charAt(i));       // last character
-  
-        
+        sb.append(num.charAt(i)); // last character
+
         return sb.toString();
-        
     }
 }

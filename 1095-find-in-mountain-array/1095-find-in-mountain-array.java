84d907fca74e0d82c36afa2527b55daa1bec466f
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-10-20 12:42:48 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (92.80%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,83 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+
+class Solution {
+
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int start = 0;
+        int end = mountainArr.length() - 1;
+
+        int peak = peak(mountainArr, start, end);
+        int idx1 = searchFirstHalf(mountainArr, target, start, peak);
+        int idx2 = searchSecondHalf(mountainArr, target, peak, end);
+
+        if(idx1 == -1) {
+            return idx2;
+        }
+        else {
+            return idx1;
+        }
+    }
+
+    public static int peak(MountainArray mountainArr, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int midVal = mountainArr.get(mid);
+
+            if (start == end) {
+                break;
+            } else if (midVal < mountainArr.get(mid + 1)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (start == end) return start;
+
+        return -1;
+    }
+
+    public static int searchFirstHalf(MountainArray mountainArr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int midVal = mountainArr.get(mid);
+
+            if (midVal == target) {
+                return mid;
+            } else if (midVal < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int searchSecondHalf(MountainArray mountainArr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            int midVal = mountainArr.get(mid);
+
+            if (midVal == target) {
+                return mid;
+            } else if (midVal > target) { // for descending just change < to >
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}

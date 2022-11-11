88042416ded503f251ce145b765f89f54fee665e
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-11-12 00:30:09 +0530

    Time: 8 ms (25.11%), Space: 45 MB (39.83%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        subsetSum(0, candidates, target, 0, list, new ArrayList<>());
+        
+        return list;
+    }
+    
+    public void subsetSum(int ind, int[] arr, int target,int sum, List<List<Integer>> list, List<Integer> ds) {
+        
+        if(sum == target) {
+            list.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        if(ind == arr.length || sum > target) return;
+
+        
+        for(int i=ind; i<arr.length; i++) {
+            
+            ds.add(arr[i]);
+            sum += arr[i];
+            subsetSum(i, arr, target, sum, list, ds);
+            ds.remove(ds.size()-1);
+            sum -= arr[i];
+            
+//             subsetSum(i+1, arr, target, sum, list, ds);
+        }
+    }
+}

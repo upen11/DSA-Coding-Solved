3658add7aa950e01f1088f9bd48adc619fb1d31b
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-11-16 14:01:36 +0530

    Time: 1 ms (26.95%), Space: 49.7 MB (57.37%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -18,9 +18,19 @@ class Solution {
         
         if(root == null) return 0;
         
-        int l = countNodes(root.left);
-        int r = countNodes(root.right);
+        int  height = 1;
+        TreeNode l = root, r = root;
+        while(l.left != null && r.right != null) {
+           height++;  
+            l = l.left;
+            r = r.right;
+        }
         
-        return 1+l+r;
+        if(l==null && r==null) {
+            return (int)Math.pow(2, height) - 1;
+        }
+        else {
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
     }
 }

e8ae0e40dcba8fe9917f9a385c58a1a9a274226b
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-11-17 11:45:17 +0530

    Time: 6 ms (39.43%), Space: 44.9 MB (53.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,32 +1,34 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(candidates);
-        subsetSum(0, candidates, target, 0, list, new ArrayList<>());
-        
-        return list;
-    }
     
-    public void subsetSum(int ind, int[] arr, int target,int sum, List<List<Integer>> list, List<Integer> ds) {
+    public static void subsets(int[] arr, int ind, int sum, int target, List<List<Integer>> subset, ArrayList<Integer> list) {
         
         if(sum == target) {
-            list.add(new ArrayList<>(ds));
+            subset.add(new ArrayList<>(list));
             return;
         }
+        else if(ind >= arr.length || sum > target) {
+            return;
+        }
+        else {
+            for(int i=ind; i<arr.length; i++) {
+                
+                sum += arr[i];
+                list.add(arr[i]);
+                subsets(arr, i, sum, target, subset, list);
+                sum -= arr[i];
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
-        if(ind == arr.length || sum > target) return;
-
+        Arrays.sort(candidates);
         
-        // for(int i=ind; i<arr.length; i++) {
-            
-            ds.add(arr[ind]);
-            sum += arr[ind];
-            subsetSum(ind, arr, target, sum, list, ds);
-            ds.remove(ds.size()-1);
-            sum -= arr[ind];
-            
-            subsetSum(ind+1, arr, target, sum, list, ds);
-        // }
+        List<List<Integer>> subset = new ArrayList<>();
+        
+        subsets(candidates, 0, 0, target, subset, new ArrayList<>());
+        
+        return subset;
     }
 }

13fef428385b3ade6176140e5ac09071deba2971
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-11-22 19:52:44 +0530

    Time: 8 ms (54.14%), Space: 44.3 MB (41.41%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,45 +1,33 @@
 class Solution {
-
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
-
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            stack1.push(nums2[i]);
+        
+        Map<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i=0; i<nums2.length; i++) {
+            
+           hm.put(nums2[i], i);
         }
-
-        for (int i = 0; i < nums1.length; i++) {
-            while (!stack1.isEmpty()) {
-                int n = stack1.pop();
-
-                if (nums1[i] != n) {
-                    stack2.push(n);
-                } else if (nums1[i] == n) {
-                    stack2.push(n);
-                    boolean flag = false;
-                    while (!stack1.isEmpty()) {
-                        int v = stack1.pop();
-                        if (v > n) {
-                            stack2.push(v);
-                            nums1[i] = v;
-                            flag = true;
-                            break;
-                        } else {
-                            stack2.push(v);
-                        }
-                    }
-
-                    if (flag == false && stack1.isEmpty()) {
-                        nums1[i] = -1;
-                    }
+        
+        for(int i=0; i<nums1.length; i++) {
+            
+            int j = hm.get(nums1[i]);
+            int val = nums2[j];
+            boolean flag = false;
+            for(; j<nums2.length-1; j++) {
+                
+                if(nums2[j+1] > val) {
+                    nums1[i] = nums2[j+1];
+                    flag = true;
+                    break;
                 }
             }
-
-            while (!stack2.isEmpty()) {
-                stack1.push(stack2.pop());
+            
+            if(flag == false) {
+                nums1[i] = -1;
             }
         }
-
+        
         return nums1;
+        
     }
 }

1b839404a16156417143b074ef589f4cf325992c
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-11-23 00:24:48 +0530

    Time: 11 ms (20.83%), Space: 44.3 MB (41.41%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,33 +1,66 @@
 class Solution {
+
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        Map<Integer, Integer> hm = new HashMap<>();
-        
-        for(int i=0; i<nums2.length; i++) {
-            
-           hm.put(nums2[i], i);
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        Stack<Integer> monotoneStack = new Stack<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            hm.put(nums1[i], i);
         }
-        
-        for(int i=0; i<nums1.length; i++) {
-            
-            int j = hm.get(nums1[i]);
-            int val = nums2[j];
-            boolean flag = false;
-            for(; j<nums2.length-1; j++) {
-                
-                if(nums2[j+1] > val) {
-                    nums1[i] = nums2[j+1];
-                    flag = true;
-                    break;
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            if (monotoneStack.isEmpty()) {
+                monotoneStack.push(nums2[i]);
+
+                if (hm.containsKey(nums2[i])) nums1[hm.get(nums2[i])] = -1;
+            } else if (nums2[i] > monotoneStack.peek()) {
+                while (!monotoneStack.isEmpty() && nums2[i] > monotoneStack.peek()) {
+                    monotoneStack.pop();
                 }
-            }
-            
-            if(flag == false) {
-                nums1[i] = -1;
+
+                if (!monotoneStack.isEmpty()) {
+                    if (hm.containsKey(nums2[i])) {
+                        nums1[hm.get(nums2[i])] = monotoneStack.peek();
+                    }
+                } else {
+                    if (hm.containsKey(nums2[i])) {
+                        nums1[hm.get(nums2[i])] = -1;
+                    }
+                }
+
+                monotoneStack.push(nums2[i]);
+            } else if (nums2[i] < monotoneStack.peek()) {
+                if (hm.containsKey(nums2[i])) {
+                    nums1[hm.get(nums2[i])] = monotoneStack.peek();
+                }
+
+                monotoneStack.push(nums2[i]);
             }
         }
-        
+
         return nums1;
-        
     }
 }
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         Map<Integer, Integer> hm = new HashMap<>();
+//         for(int i=0; i<nums2.length; i++) {
+//            hm.put(nums2[i], i);
+//         }
+//         for(int i=0; i<nums1.length; i++) {
+//             int j = hm.get(nums1[i]);
+//             int val = nums2[j];
+//             boolean flag = false;
+//             for(; j<nums2.length-1; j++) {
+//                 if(nums2[j+1] > val) {
+//                     nums1[i] = nums2[j+1];
+//                     flag = true;
+//                     break;
+//                 }
+//             }
+//             if(flag == false) {
+//                 nums1[i] = -1;
+//             }
+//         }
+//         return nums1;
+//     }
+// }

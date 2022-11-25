c06dae251c5ecdfed3f1391023cf125fb75e600c
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-11-25 22:23:26 +0530

    Time: 16 ms (22.21%), Space: 45.8 MB (7.35%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public String simplifyPath(String path) {
+        StringBuilder sb = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < path.length(); i++) {
+            // char ch = path.charAt(i);
+
+            while (i < path.length() && path.charAt(i) != '/') {
+                sb.append(path.charAt(i));
+                i++;
+            }
+
+            // System.out.println(sb);
+            if (sb.length() != 0) {
+                // System.out.println(sb);
+                // System.out.println(sb.toString().equals(".."));
+                if (sb.toString().equals("..") && !stack.isEmpty()) {
+                    stack.pop();
+                } else if (sb.toString().equals("..") && stack.isEmpty()) {
+                    // continue is wrong, here do nothing;
+                } else if (!sb.toString().equals(".")) {
+                    stack.push(sb.toString());
+                }
+            }
+
+            sb.setLength(0);    // emptying sb
+        }
+
+        // System.out.println(stack);
+
+        String dir = "/";
+        
+        if (!stack.isEmpty()) 
+            dir += stack.pop();
+        
+        while (!stack.isEmpty()) {
+            // System.out.println(dir);
+            dir = "/" + stack.pop() + dir;
+            // System.out.println(st);
+        }
+
+        return dir;
+    }
+}

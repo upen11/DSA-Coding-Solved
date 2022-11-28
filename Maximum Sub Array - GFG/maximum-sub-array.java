5edc914ea3ac2459246a65c2a909a31632e0af99
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-11-28 21:07:21 +0530

    Added solution - LeetHub

--- Maximum Sub Array - GFG/maximum-sub-array.java
+++ Maximum Sub Array - GFG/maximum-sub-array.java
@@ -34,32 +34,34 @@ class GFG {
 
 class Solution {
 
+    // 8
+    // 1 2 5 -7 2 3 2 1
     ArrayList<Integer> findSubarray(int a[], int n) {
         // code here
         
         ArrayList<Integer> list = new ArrayList<>();
         
-        long sum = 0, maxSum = Integer.MIN_VALUE;
+        long sum = 0, maxSum = Integer.MIN_VALUE;   // initailized as Min Value
         int len = 0, maxLen = 0;
         int startInd = 0, endInd = 0;
     
         for(int i=0; i<n; i++) {
             
-            if(a[i] < 0) {
+            if(a[i] < 0) {  // -ve values
                 sum = 0;
-                len=0;
+                len=0;  
                 continue;
             }
             
             sum += a[i];
             len++;
             
-            if(a[i] == 0) {
-                sum++;
-            }
-            
             if(sum > maxSum) {
                 maxSum = sum;
+                endInd = i;
+                maxLen = len;
+            }
+            else if (sum == maxSum && len > maxLen) {  // I am checking the length of subarray when sum is equal
                 endInd = i;
                 maxLen = len;
             }
@@ -67,9 +69,9 @@ class Solution {
         }
         
 
-        startInd = endInd - maxLen + 1;
+        startInd = endInd - maxLen + 1; // idea is to get starting of the subarray using last index and length of subarray
         
-        if(maxSum < 0) {
+        if(maxSum < 0) {   //  as max sum will only get updated in case of non-negative value
             list.add(-1);
             return list;
         }

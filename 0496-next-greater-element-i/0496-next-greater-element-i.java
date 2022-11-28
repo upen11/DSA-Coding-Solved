eada4705175e6fd5471ac2dd70442dad1a3418d2
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-11-29 01:12:43 +0530

    Time: 3 ms (96.63%), Space: 42.5 MB (84.78%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,60 +1,41 @@
 class Solution {
-
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
         HashMap<Integer, Integer> hm = new HashMap<>();
-        Stack<Integer> monotoneStack = new Stack<>();
-
-        for (int i = 0; i < nums1.length; i++) {
-            hm.put(nums1[i], i);
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0; i<nums1.length; i++) {
+            hm.put(nums1[i], -1);
         }
+ 
+        // maintain a hash map to map nums1 array
+        // traversing nums2 from left
+        // pushing indexes in stack
+        // if next element is greater than top, ans[st.pop()] = nums[index]
+        // updating in map
+        // use while inside for loop eg [1,3,5,2,4]
+                                    // [6,5,4,3,2,1,7]
+        // expected [7,7,7,7,7]
 
-        // monotoneStack.push(nums2[nums2.length - 1]);
-
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!monotoneStack.isEmpty() && nums2[i] > monotoneStack.peek()) {
-                int nge = monotoneStack.pop(); // next greater element
-
-                if (hm.containsKey(nums2[i])) {
-                    nums1[hm.get(nums2[i])] = nge;
+        
+        for(int i=0; i<nums2.length; i++) {
+            
+            while(!stack.isEmpty() && (nums2[i] > nums2[stack.peek()]) ) {
+                int k = stack.pop();
+                // nums2[0] 1 -> nums2[1] 3
+                if(hm.containsKey(nums2[k])) {
+                    hm.put(nums2[k], nums2[i]);
                 }
             }
-
-            if (monotoneStack.isEmpty()) {
-                if (hm.containsKey(nums2[i])) {
-                    nums1[hm.get(nums2[i])] = -1;
-                }
-            } else {
-                if (hm.containsKey(nums2[i])) {
-                    nums1[hm.get(nums2[i])] = monotoneStack.peek();
-                }
-            }
-
-            monotoneStack.push(nums2[i]);
+            
+            stack.push(i);
         }
-
+        
+        for(int i=0; i<nums1.length; i++) {
+                nums1[i] = hm.get(nums1[i]);
+        }
+        
         return nums1;
     }
 }
-//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-//         Map<Integer, Integer> hm = new HashMap<>();
-//         for(int i=0; i<nums2.length; i++) {
-//            hm.put(nums2[i], i);
-//         }
-//         for(int i=0; i<nums1.length; i++) {
-//             int j = hm.get(nums1[i]);
-//             int val = nums2[j];
-//             boolean flag = false;
-//             for(; j<nums2.length-1; j++) {
-//                 if(nums2[j+1] > val) {
-//                     nums1[i] = nums2[j+1];
-//                     flag = true;
-//                     break;
-//                 }
-//             }
-//             if(flag == false) {
-//                 nums1[i] = -1;
-//             }
-//         }
-//         return nums1;
-//     }
-// }

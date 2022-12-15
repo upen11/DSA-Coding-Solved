6376e2fa5a810bfff8239623411c975a482146e1
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-12-15 19:14:08 +0530

    Time: 0 ms (100.00%), Space: 40 MB (79.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,19 @@
 class Solution {
 
-    public static int rob(int[] nums) {
-        int ifRobbedPrevious = 0; // max monney can get if rob current house
-        int ifDidntRobPrevious = 0; // max money can get if not rob current house
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length];
 
-        // We go through all the values, we maintain two counts, 1) if we rob this cell, 2) if we didn't rob this cell
-        for (int i = 0; i < nums.length; i++) {
-            // If we rob current cell, previous cell shouldn't be robbed. So, add the current value to previous one.
-            int currRobbed = ifDidntRobPrevious + nums[i];
+        dp[0] = nums[0];
+        if (nums.length == 1) return dp[0];
 
-            // If we don't rob current cell, then the count should be max of the previous cell robbed and not robbed
-            int currNotRobbed = Math.max(ifDidntRobPrevious, ifRobbedPrevious);
+        dp[1] = Math.max(nums[0], nums[1]);
+        if (nums.length == 2) return dp[1];
 
-            // Update values for the next round
-            ifDidntRobPrevious = currNotRobbed;
-            ifRobbedPrevious = currRobbed;
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2]);
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i]);
         }
 
-        return Math.max(ifRobbedPrevious, ifDidntRobPrevious);
+        return dp[nums.length - 1];
     }
 }

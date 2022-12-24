3f4e1ee7198be9a8af646c9463a9b3a4ebdeb853
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-12-25 00:01:28 +0530

    Time: 1 ms (90.34%), Space: 39.6 MB (74.88%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -0,0 +1,16 @@
+class Solution {
+   int numTilings(int N) 
+    {
+        long[] g = new long[1001], u = new long[1001];
+        int mod=1000000007;
+        g[0]=0; g[1]=1; g[2]=2;
+        u[0]=0; u[1]=1; u[2]=2;
+        
+        for(int i=3;i<=N;i++)
+        {
+            u[i] = (u[i-1] + g[i-1]           )   %mod;
+            g[i] = (g[i-1] + g[i-2] + 2*u[i-2])   %mod;
+        }
+        return (int)g[N]%mod;
+    }
+}

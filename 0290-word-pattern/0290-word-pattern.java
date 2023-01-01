d65dbc0feab7fc1e467b7f8d791314b8ebe1a408
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-01-01 22:14:37 +0530

    Time: 1 ms (95.36%), Space: 40 MB (95.05%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public boolean wordPattern(String pattern, String s) {
+        String arr[] = s.split(" ");
+
+        if (pattern.length() != arr.length) return false;
+
+        Map<Character, String> map = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+
+            if (map.containsKey(ch)) {
+                if (!map.get(ch).equals(arr[i])) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(arr[i])) 
+                    return false;
+                map.put(ch, arr[i]);
+            }
+        }
+
+        return true;
+    }
+}

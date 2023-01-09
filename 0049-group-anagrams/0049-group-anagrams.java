5b5c6e036835183766e41ee1421546835f04b4f2
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-01-09 23:13:16 +0530

    Time: 60 ms (12.46%), Space: 49.7 MB (58.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,10 +1,50 @@
 class Solution {
 
+     // T: O(N * M)
+    // S: O(N)
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+
+        HashMap<HashMap<Character, Integer>, ArrayList<String>> bigMap = new HashMap<>();
+
+        // bigMap
+        // smallMap
+        // "eat" --> e = 1, a = 1, t = 1
+
+        for (String s : strs) {
+            HashMap<Character, Integer> smallMap = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                smallMap.put(ch, smallMap.getOrDefault(ch, 0) + 1);
+            }
+            if (bigMap.containsKey(smallMap)) {
+                ArrayList list1 = bigMap.get(smallMap);
+                list1.add(s);
+                bigMap.put(smallMap, list1);
+            } else {
+                ArrayList list2 = new ArrayList<>();
+                list2.add(s);
+                bigMap.put(smallMap, list2);
+            }
+        }
+
+        // System.out.println(bigMap.values());
+
+        // traversing map values
+        for (ArrayList<String> a : bigMap.values()) {
+            ans.add(a);
+        }
+
+        return ans;
+    }
+    
+    
+    // T: O(N log N)
+    // S: O(N)
+    /*
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> ans = new ArrayList<>();
         HashMap<String, List<String>> map = new HashMap<>();
-
-        for (int i = 0; i < strs.length; i++) {}
 
         for (String s : strs) {
             char[] arr = s.toCharArray();
@@ -29,4 +69,5 @@ class Solution {
 
         return ans;
     }
+    */
 }

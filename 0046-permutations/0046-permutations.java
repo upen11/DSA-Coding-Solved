f7241232f14dd37bbeeae02d588e731ef2227ce8
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-01-31 19:41:39 +0530

    Time: 1 ms (92.23%), Space: 42.3 MB (94.21%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,12 +3,12 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
-        backtracking(nums, 0, result, new ArrayList<>());
+        backtracking(nums, result, new ArrayList<>());
 
         return result;
     }
 
-    public void backtracking(int[] nums, int idx, List<List<Integer>> result, List<Integer> temp) {
+    public void backtracking(int[] nums, List<List<Integer>> result, List<Integer> temp) {
         if (temp.size() == nums.length) {
             result.add(new ArrayList<>(temp));
             return;
@@ -20,7 +20,7 @@ class Solution {
             temp.add(nums[i]);
 
             // System.out.println(temp);
-            backtracking(nums, i + 1, result, temp);
+            backtracking(nums, result, temp);
             temp.remove(temp.size() - 1);
         }
     }

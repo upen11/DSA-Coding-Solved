eb9d206f64e3b01d1439b6f696e63ce3527a2c59
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-02-04 23:59:57 +0530

    Time: 27 ms (37.92%), Space: 43.5 MB (18.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -4,19 +4,33 @@ class Solution {
         if (s1.length() > s2.length()) return false;
 
         Map<Character, Integer> s1Freq = new HashMap<>();
+        Map<Character, Integer> s2Freq = new HashMap<>();
+
+        int p1 = 0;
+        int p2 = s1.length() - 1;
 
         for (int i = 0; i < s1.length(); i++) {
             s1Freq.put(s1.charAt(i), s1Freq.getOrDefault(s1.charAt(i), 0) + 1);
+            s2Freq.put(s2.charAt(i), s2Freq.getOrDefault(s2.charAt(i), 0) + 1);
         }
 
-        for (int i = 0; i < s2.length() - s1.length() + 1; i++) {
-            Map<Character, Integer> s2Freq = new HashMap<>();
+        if (s2Freq.get(s2.charAt(p2)) == 1) 
+            s2Freq.remove(s2.charAt(p2)); 
+        else 
+            s2Freq.put(s2.charAt(p2), s2Freq.getOrDefault(s2.charAt(p2), 0) - 1);
 
-            for (int j = i; j < i + s1.length(); j++) {
-                s2Freq.put(s2.charAt(j), s2Freq.getOrDefault(s2.charAt(j), 0) + 1);
-            }
+        for (int i = 0; i < s2.length() - s1.length() + 1; i++) {
+            s2Freq.put(s2.charAt(p2), s2Freq.getOrDefault(s2.charAt(p2), 0) + 1);
 
             if (matches(s1Freq, s2Freq)) return true;
+
+            if (s2Freq.get(s2.charAt(p1)) == 1) 
+                s2Freq.remove(s2.charAt(p1)); 
+            else 
+                s2Freq.put(s2.charAt(p1), s2Freq.getOrDefault(s2.charAt(p1), 0) - 1);
+
+            p1++;
+            p2++;
         }
 
         return false;

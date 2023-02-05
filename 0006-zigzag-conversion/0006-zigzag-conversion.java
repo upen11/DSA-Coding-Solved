c2a43cae23df7d948bb0b1d73f01e7add5140490
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-02-06 00:51:58 +0530

    Time: 3 ms (94.51%), Space: 42.1 MB (98.19%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,41 +1,83 @@
 class Solution {
+
     public String convert(String s, int numRows) {
         
         if(numRows == 1) return s;
         
-         StringBuilder res = new StringBuilder();
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<numRows; i++) {
+ 
+            boolean flag = false;
+            int jump = 0;
+            int cut = 0;
+            int index = i;
+            while(index < n) {
+                if(i == 0 || i == numRows-1) {
+                    sb.append(s.charAt(index));
+                    jump = 2 * (numRows - 1);
+                }
+                else {
+                    sb.append(s.charAt(index));
+                    if(flag == false) {
+                        cut = 2 * i;
+                        jump = 2 * (numRows - 1) - cut;
+                        flag = true;
+                    }
+                    else {
+                        jump = cut;
+                        flag = false;
+                    }
+                    
+                }
+                index += jump;
+            }
+        }
         
+        return sb.toString();
+    }
+    
+    
+    /*
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+
+        StringBuilder res = new StringBuilder();
+
         // will make numRows StringBuilders then at last club them
         // row wise storing in string builders array
         StringBuilder[] sbs = new StringBuilder[numRows];
-        
-        for(int i=0; i<numRows; i++) {
+
+        for (int i = 0; i < numRows; i++) {
             sbs[i] = new StringBuilder();
         }
-        
+
         // index will point to string
         int index = 0;
         int n = s.length();
-        
+
         // Traverse ZigZag
-        while(index < n) {
+        while (index < n) {
             // go down
-            for(int j=0; j<numRows && index < n; j++) {
+            for (int j = 0; j < numRows && index < n; j++) {
                 sbs[j].append(s.charAt(index++));
             }
-            
+
             // go up from row-2 i.e second last row
-            for(int j=numRows-2; j>0 && index < n; j--) {
+            for (int j = numRows - 2; j > 0 && index < n; j--) {
                 sbs[j].append(s.charAt(index++));
             }
         }
         
-       
-       // at last clubbing whole string builder array into 1 string builder 
-        for(int i=0; i<numRows; i++) {
+        System.out.print(sbs[1].toString());
+
+        // at last clubbing whole string builder array into 1 string builder
+        for (int i = 0; i < numRows; i++) {
             res.append(sbs[i]);
         }
-        
+
         return res.toString();
     }
+    
+    */
 }

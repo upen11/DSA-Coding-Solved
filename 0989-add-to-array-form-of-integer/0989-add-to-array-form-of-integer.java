05b7bdb0312726a80d740ddb4fbb7dbc73072258
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-02-15 23:35:58 +0530

    Time: 4 ms (84.63%), Space: 44.1 MB (35.29%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -5,37 +5,13 @@ class Solution {
 
         int len = num.length - 1;
         int sum = 0;
-        int carry = 0;
-        while (len >= 0) {
-            int d = 0;
-            if (k > 0) {
-                d = k % 10;
-                k = k / 10;
+        while ((len >= 0) || (k > 0)) {
+            if (len >= 0) {
+                k = num[len--] + k;
             }
-
-            sum = num[len--] + d + carry;
-            carry = 0;
-            if (sum > 9) {
-                sum = (sum + carry) % 10;
-                carry = 1;
-            }
-            list.add(sum);
-        }
-
-        while (k > 0) {
-            sum = k % 10 + carry;
-            if (sum > 9) {
-                sum = sum % 10;
-                carry = 1;
-            } else {
-                carry = 0;
-            }
+            sum = k % 10;
             list.add(sum);
             k = k / 10;
-        }
-
-        if (carry != 0) {
-            list.add(1);
         }
 
         Collections.reverse(list);

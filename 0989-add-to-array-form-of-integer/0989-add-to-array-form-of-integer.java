3649f6a406c10f4f27db5dd9f928489e2733d9c3
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-02-15 23:23:45 +0530

    Time: 3 ms (97.70%), Space: 43.9 MB (55.33%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,52 +1,39 @@
 class Solution {
 
     public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> list = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
 
         int len = num.length - 1;
         int sum = 0;
         int carry = 0;
         while (len >= 0) {
+            int d = 0;
             if (k > 0) {
-                // System.out.println("k :"+k);
-                // System.out.println("carry :"+ carry);
-                int d = k % 10;
-                sum = (num[len--] + d + carry);
-                // System.out.println("sum :"+ sum);
-                carry = 0;
-                if (sum > 9) {
-                    sum = (sum + carry) % 10;
-                    carry = 1;
-                }
-                list.add(sum);
-
+                d = k % 10;
                 k = k / 10;
-            } else {
-                sum = num[len--] + carry;
-                if (sum > 9) {
-                    sum = sum % 10;
-                    carry = 1;
-                } else {
-                    carry = 0;
-                }
-
-                list.add(sum);
             }
+
+            sum = num[len--] + d + carry;
+            carry = 0;
+            if (sum > 9) {
+                sum = (sum + carry) % 10;
+                carry = 1;
+            }
+            list.add(sum);
         }
 
         while (k > 0) {
             sum = k % 10 + carry;
-            if(sum > 9) {
+            if (sum > 9) {
                 sum = sum % 10;
                 carry = 1;
-            }
-            else {
+            } else {
                 carry = 0;
             }
             list.add(sum);
             k = k / 10;
         }
-        
+
         if (carry != 0) {
             list.add(1);
         }

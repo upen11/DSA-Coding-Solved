a673f6e487f10427594cd7cce320a4d4afdce19e
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-02-20 23:03:53 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (92.74%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -14,57 +14,25 @@
  * }
  */
 class Solution {
-
-    public int countNodes(TreeNode root) {
-        if (root == null) return 0;
-
-        return 1 + countNodes(root.left) + countNodes(root.right);
-    }
-
-    public boolean dfs(TreeNode root, int index, int totalNodes) {
-        if (root == null) return true;
-
-        if (index > totalNodes) return false;
-
-        return dfs(root.left, 2 * index, totalNodes) && dfs(root.right, 2 * index + 1, totalNodes);
-    }
-
-    // DFS
     public boolean isCompleteTree(TreeNode root) {
-        // Total number of nodes
-        // DFS --> L.C 2 * i
-        //         R.C 2 * i + 1
         
-        // if (index > totalNodes) means not complete binary tree
-
-        int totalNodes = countNodes(root);
-
-        return dfs(root, 1, totalNodes);
+        int nodes = count(root);
+        
+        return dfs(root, 1, nodes);
     }
     
-    // BFS
-    /*
-    public boolean isCompleteTree(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-
-        queue.add(root);
-
-        boolean pastNull = false;
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.remove();
-
-            if (pastNull == true && node != null) return false;
-
-            if (node != null) {
-                queue.add(node.left);
-                queue.add(node.right);
-            }
-            else {
-                pastNull = true;
-            }
-        }
-
-        return true;
+    public int count(TreeNode root) {
+        if(root == null) return 0;
+        
+        return 1 + count(root.left) + count(root.right);
     }
-    */
+    
+    public boolean dfs(TreeNode root, int ind, int nodes) {
+        if(root == null) return true;
+        
+        if(ind > nodes) return false;
+        
+        return dfs(root.left, ind * 2, nodes) && dfs(root.right, ind * 2 + 1, nodes);
+        
+    }
 }

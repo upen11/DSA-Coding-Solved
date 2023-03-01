bb904b5c79f09cae62fdfb8e3eddd67b2e17b78c
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-03-01 23:42:32 +0530

    Time: 34 ms (62.37%), Space: 53.7 MB (42.42%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,49 +1,55 @@
 class Solution {
 
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
-
+        
+        mergeSort(nums, 0, nums.length-1);
+        
         return nums;
-    }
-
+}
+    
     public void mergeSort(int[] nums, int low, int high) {
-        if (low >= high) return;
-
+        
+        if(low == high) return;
+        
         int mid = low + (high - low) / 2;
-
+        
         mergeSort(nums, low, mid);
-        mergeSort(nums, mid + 1, high);
-
+        mergeSort(nums, mid+1, high);
+        
         merge(nums, low, mid, high);
+        
     }
-
+    
     public void merge(int[] nums, int low, int mid, int high) {
+        
         int i = low;
         int j = mid + 1;
-
+        
         int[] temp = new int[high - low + 1];
         int k = 0;
-
-        while (i <= mid && j <= high) {
-            if (nums[i] < nums[j]) {
+        
+        while(i <= mid && j <= high ) {
+            if(nums[i] < nums[j]) {
                 temp[k] = nums[i++];
-            } else {
+            }
+            else {
                 temp[k] = nums[j++];
             }
             k++;
         }
-
-        while (i <= mid) {
+        
+        while(i <= mid) {
             temp[k++] = nums[i++];
         }
-
-        while (j <= high) {
+        
+        while(j <= high) {
             temp[k++] = nums[j++];
         }
-
-        int y = 0;
-        for (int z = low; z <= high; z++) {
+        
+        int y=0;
+        for(int z=low; z<=high; z++) {
             nums[z] = temp[y++];
         }
     }
+    
 }

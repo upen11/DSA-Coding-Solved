4ad8b12b06393f09130f72ba97f0c9ceb8f8e068
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-03-06 14:29:24 +0530

    Time: 1 ms (100.00%), Space: 43 MB (46.15%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
@@ -1,20 +1,21 @@
 class Solution {
     public int findLengthOfLCIS(int[] arr) {
         
+        if(arr.length == 1) return 1;
+        
         int count = 1;
         int maxCount = 0;
         for(int i=0; i<arr.length-1; i++) {
             if(arr[i] < arr[i+1]) {
-                count++;
-                maxCount = Math.max(maxCount, count);
+                count++;  
             }
             else {
                 count = 1;
             }
+            
+            maxCount = Math.max(maxCount, count);
         }
-        
-        maxCount = Math.max(maxCount, count);
-        
+                
         return maxCount;
     }
 }

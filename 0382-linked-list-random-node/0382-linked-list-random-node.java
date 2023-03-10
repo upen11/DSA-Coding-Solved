8716229354e28621f475b651e62024e716a22a6e
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-03-10 20:37:35 +0530

    Time: 16 ms (12.55%), Space: 44.4 MB (55.18%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.java
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -9,7 +9,37 @@
  * }
  */
 class Solution {
+    // T.N O(N)
+    // S.C O(1)
+    // Reservoir Sampling
+    ListNode newHead = null;
 
+    public Solution(ListNode head) {
+        newHead = head;
+    }
+
+    public int getRandom() {
+        Random random = new Random();
+
+        ListNode curr = newHead;
+        int i = 1;
+        int ans = 0;
+        while (curr != null) {
+            int r = random.nextInt(10001);
+            if (r % i == 0) {   // this will give each element a equal chance to te picked
+                ans = curr.val;
+            }
+
+            i++;
+            curr = curr.next;
+        }
+
+        return ans;
+    }
+    // T.N : O(N)
+    // S.C : O(N)
+
+    /* 
     List<Integer> list = null;
     public Solution(ListNode head) {
         list = new ArrayList<>();
@@ -25,8 +55,8 @@ class Solution {
         int r = random.nextInt(list.size());
         return list.get(r);
     }
+    */
 }
-
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(head);

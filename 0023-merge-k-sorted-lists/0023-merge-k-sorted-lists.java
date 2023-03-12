be5032f4514cc8d053f918b2511ce913082eee7b
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-03-12 22:33:55 +0530

    Time: 1 ms (100.00%), Space: 44.6 MB (27.41%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,6 +10,59 @@
  */
 class Solution {
 
+    // T.C O(N * KlogK)
+    // S.C O(1)
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+
+        // 1. divide and conquer
+        // 2. mergingTwoSortedLL
+        ListNode head = divideAndConquer(lists, 0, lists.length - 1);
+
+        return head;
+    }
+
+    public ListNode divideAndConquer(ListNode[] lists, int start, int end) {
+        if (start == end) return lists[start];
+
+        int mid = start + (end - start) / 2;
+
+        ListNode l1 = divideAndConquer(lists, start, mid);
+        ListNode l2 = divideAndConquer(lists, mid + 1, end);
+
+        return mergingTwoSortedLL(l1, l2);
+    }
+
+    public ListNode mergingTwoSortedLL(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+
+        if (l2 == null) return l1;
+
+        ListNode head = l1.val <= l2.val ? l1 : l2;
+
+        ListNode prev = new ListNode(-1);
+        ListNode curr1 = l1;
+        ListNode curr2 = l2;
+
+        while (curr1 != null && curr2 != null) {
+            if (curr1.val <= curr2.val) {
+                prev.next = curr1;
+                curr1 = curr1.next;
+            } else {
+                prev.next = curr2;
+                curr2 = curr2.next;
+            }
+            prev = prev.next;
+        }
+
+        prev.next = curr1 == null ? curr2 : curr1;
+
+        return head;
+    }
+    // T.C O(K2 * N)
+    // S.C O(1)
+    /*
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists.length == 0) return null;
 
@@ -58,4 +111,5 @@ class Solution {
 
         return head;
     }
+    */
 }

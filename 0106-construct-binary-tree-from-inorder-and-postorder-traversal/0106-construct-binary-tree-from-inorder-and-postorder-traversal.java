86c3f8a717c49db33b20ce2d4cf44e65194e0ddf
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-03-17 12:15:22 +0530

    Time: 2 ms (97.39%), Space: 42.7 MB (17.83%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -16,26 +16,29 @@
 class Solution {
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return helper(inorder, postorder, 0, inorder.length - 1, 0, postorder.length - 1);
+        int inLen = inorder.length;
+        int poLen = postorder.length;
+
+        Map<Integer, Integer> inorderMap = new HashMap<>();
+
+        for (int i = 0; i < inLen; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+
+        return helper(inorderMap, postorder, 0, inLen - 1, 0, poLen - 1);
     }
 
-    public TreeNode helper(int[] inorder, int[] postorder, int inStart, int inEnd, int poStart, int poEnd) {
-        if (poStart > poEnd) return null;
+    public TreeNode helper(Map<Integer, Integer> inorderMap, int[] postorder, int inStart, int inEnd, int poStart, int poEnd) {
+        if (inStart > inEnd || poStart > poEnd) return null;
 
         TreeNode root = new TreeNode(postorder[poEnd]);
 
-        int i = 0;
-        for (i = inStart; i <= inEnd; i++) {
-            if (inorder[i] == postorder[poEnd]) {
-                break;
-            }
-        }
-
+        int i = inorderMap.get(root.val);
         int leftSize = i - inStart;
         int rightSize = inEnd - i;
 
-        root.left = helper(inorder, postorder, inStart, i - 1, poStart, poStart + leftSize - 1);
-        root.right = helper(inorder, postorder, i + 1, inEnd, poEnd - rightSize, poEnd - 1);
+        root.left = helper(inorderMap, postorder, inStart, i - 1, poStart, poStart + leftSize - 1);
+        root.right = helper(inorderMap, postorder, i + 1, inEnd, poEnd - rightSize, poEnd - 1);
 
         return root;
     }

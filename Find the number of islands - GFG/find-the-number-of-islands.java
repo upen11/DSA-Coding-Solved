02409be7f48d6f2136b1c25ad3d019e670e2a332
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2023-03-27 21:48:09 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.java
+++ Find the number of islands - GFG/find-the-number-of-islands.java
@@ -26,78 +26,31 @@ class GFG {
 }
 // } Driver Code Ends
 
-
-// We made pair class because we need to traverse in all 8 directions
-// i = row and j = col of the grid[i][j] = '1'. 
-// We will then traverse from this co-ordinate in all 9 directions
-class Pair {
-    int i;
-    int j;
-        
-    Pair(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-}
-
-// TC : O(m * n)
-// SC : O(m * n)
-
 class Solution {
     
-    // how to do traversal => we will find a node where it is '1'. Then we will do bfs in all 8 directions.
-    // if we are able to traverse then how to count islands => we will have a outer loop where we will go from 0 to m*n.
-    // we will process only those who are '1' and not visited.
-   
-    public void bfs(int r, int c, char[][] grid, boolean[][] isVisited) {
-        
-        isVisited[r][c] = true;
-        
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(r, c));
+    public void dfs(int ro, int co, char[][] grid, boolean[][] isVisited) {
+        isVisited[ro][co] = true;
         
         int rows = grid.length;
         int cols = grid[0].length;
         
-        while(!queue.isEmpty()) {
-            
-            Pair node = queue.poll();
-            
-            int row = node.i;
-            int col = node.j;
-            
-            // traversing neighbours and marking them true
-            for(int i=-1; i<=1; i++) {
-                for(int j=-1; j<=1; j++) {
-                    
-                    // neighbour row and col in 8 directions, well 9 when we have row+0 and col+0
-                    int nrow = row + i;
-                    int ncol = col + j;
-                    
-                    // valid conditions(inside matrix), need to be land and non-visited
-                    if(nrow >= 0 && nrow < rows && ncol >= 0 && ncol < cols 
-                    && grid[nrow][ncol] == '1' 
-                    && isVisited[nrow][ncol] == false) {
-                        isVisited[nrow][ncol] = true;
-                        queue.add(new Pair(nrow, ncol));
-                    }
+        for(int i=-1; i<=1; i++) {
+            for(int j=-1; j<=1; j++) {
+                int nrow = ro + i;
+                int ncol = co + j;
+                
+                if(nrow >=0 && nrow < rows && ncol >=0 && ncol < cols
+                && isVisited[nrow][ncol] == false
+                && grid[nrow][ncol] == '1') {
+                    dfs(nrow, ncol, grid, isVisited);
                 }
             }
         }
-        
     }
     
     // Function to find the number of islands.
     public int numIslands(char[][] grid) {
         // Code here
-        // {
-        // {0,1,1,1,0,0,0},
-        // {0,0,1,1,0,1,0}
-        // }
-        // given a 2D matrix
-        // we need a visited array
-        // then we need to do bfs/dfs in all 8 directions where we can find connecting 1's
-        
         int count = 0;
         
         int rows = grid.length;
@@ -107,11 +60,9 @@ class Solution {
         
         for(int i=0; i<rows; i++) {
             for(int j=0; j<cols; j++) {
-                char node = grid[i][j];
-                
-                if(isVisited[i][j] == false && node == '1') {
+                if(isVisited[i][j] == false && grid[i][j] == '1') {
                     count++;
-                    bfs(i, j, grid, isVisited); // bfs means to work with Queue and see neighbours
+                    dfs(i, j, grid, isVisited);
                 }
             }
         }
